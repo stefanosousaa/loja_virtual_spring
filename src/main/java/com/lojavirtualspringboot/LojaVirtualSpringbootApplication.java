f97d123dca7015317eaf90a33444aee23c03030b
Stefano Sousa <stefanosousa@gmail.com>
2024-12-15 00:12:14 -0300

    commit inicial do projeto

---
+++ src/main/java/com/lojavirtualspringboot/LojaVirtualSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.lojavirtualspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lojavirtualspringboot/LojaVirtualSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lojavirtualspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
